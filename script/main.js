2846709fbc25e298b474eb3c7ca63b9bfe5a8901
Felix Yudashkin <ihbobuacc@gmail.com>
2023-02-14 18:48:58 +0200

    Add filling to the grid

--- script/main.js
+++ script/main.js
@@ -1,12 +1,29 @@
 const container = document.getElementById("main-container");
 
-function makeRows(rows,cols){
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (let i = 0; i < rows*cols; i++) {
-        let cell = document.createElement("div");
-        container.appendChild(cell).className = "grid-item";
+function makeRows(value){
+    container.style.display = "grid";
+    container.style.gridTemplateColumns = `repeat(${value},1fr)`
+    container.style.gridTemplateRows = `repeat(${value},1fr)`
+    for (let i = 0; i < value*value; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cellDiv");
+        cell.style.border = "0.5px solid rgb(200,200,200)";
+        container.appendChild(cell);
     }
 }
 
-makeRows (16,16);
+makeRows (16);
+const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+
+
+const cells = document.getElementsByClassName("cellDiv");
+Array.from(cells).forEach(function(cell){
+    cell.addEventListener('mouseover', function() {
+        const r = randomBetween(0, 255);
+        const g = randomBetween(0, 255);
+        const b = randomBetween(0, 255);
+        cell.style.backgroundColor = `rgb(${r},${g},${b})`;
+    }
+)});
+
+// cell.style.backgroundColor = `rgb(${r},${g},${b})`;
